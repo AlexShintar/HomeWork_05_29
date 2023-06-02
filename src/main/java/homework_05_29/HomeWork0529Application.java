feea35a4cefcbfffe0f710d6fe4b0bb87a6116c4
Ash <alex@shintar.ru>
2023-06-02 14:06:36 +0300

    Initial commit

---
+++ src/main/java/homework_05_29/HomeWork0529Application.java
@@ -0,0 +1,13 @@
+package homework_05_29;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork0529Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWork0529Application.class, args);
+    }
+
+}
